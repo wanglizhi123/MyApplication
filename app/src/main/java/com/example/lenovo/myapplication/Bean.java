9b58132cdd7af4d8fc8b88ac5057f53b294b64d0
wanglizhi123 <649829200@qq.com>
2017-09-12 09:40:09 +0800

    第二次修改 2017-9-12 09:28:41

---
+++ app/src/main/java/com/example/lenovo/myapplication/Bean.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.myapplication;
+
+import android.util.Log;
+
+/**
+ * Created by lenovo on 2017/9/12.
+ */
+
+public class Bean {
+    public void data(){
+        Log.e("TAG", "data:第二次修改 " );
+    }
+}
